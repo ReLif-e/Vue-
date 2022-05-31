a791d148386231ff8dbed54c9446f4b25ed81935
baito <baito@.cn>
2022-05-31 17:48:36 +0800

    树状图

--- src/views/permissions/permissions.vue
+++ src/views/permissions/permissions.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="department-container">
-    <div class="app-container">
-      <el-card>
-        <!-- 具体页面结构 -->
-        组织架构
-      </el-card>
-    </div>
-  </div>
+  <div />
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
