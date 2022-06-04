d9b0d0361e8d5c68857acb6358f30fcab9554199
baito <baito@.cn>
2022-06-04 19:26:06 +0800

    渲染角色列表

--- src/api/employees.js
+++ src/api/employees.js
@@ -1,11 +1,23 @@
 import request from '@/utils/request'
 /**
  *
- * @returns 获取公司全部人的名称
+ * @returns 获取公司全部人的名称下拉框
  */
 export function GetEmployess() {
   return request({
     method: 'get',
     url: '/sys/user/simple'
+  })
+}
+
+/**
+ *
+ * @returns 获取员工列表,渲染表单
+ */
+export function Getemploy(params) {
+  return request({
+    method: 'get',
+    url: '/sys/user',
+    params
   })
 }
--- src/views/employees/employees.vue
+++ src/views/employees/employees.vue
@@ -1,27 +1,98 @@
 <template>
   <div>
-    <page-tool>
-      <!-- 左侧 -->
-      <template #left>
-        <i class="el-icon-info" />
-        <span>总共10条数据</span>
-      </template>
+    <div>
+      <page-tool>
+        <!-- 左侧 -->
+        <template #left>
+          <i class="el-icon-info" />
+          <span>总共{{ total }}条数据</span>
+        </template>
 
-      <!-- 右侧 -->
-      <template #right>
-        <el-button round size="small" type="primary">导入Excel</el-button>
-        <el-button round size="small" type="warning">导出Excel</el-button>
-        <el-button round size="small" type="success">新增员工</el-button>
-      </template>
-    </page-tool>
+        <!-- 右侧 -->
+        <template #right>
+          <el-button round size="small" type="primary">导入Excel</el-button>
+          <el-button round size="small" type="warning">导出Excel</el-button>
+          <el-button round size="small" type="success">新增员工</el-button>
+        </template>
+      </page-tool>
+    </div>
+    <el-card>
+      <el-table :data="Employees">
+
+        <el-table-column label="序号" type="index" />
+        <el-table-column label="姓名" prop="username" />
+        <el-table-column label="头像">
+          <template v-slot="{row}">
+            <img class="num" :src="row.staffPhoto" alt="">
+          </template>
+        </el-table-column>
+        <el-table-column label="工号" prop="workNumber" />
+        <el-table-column label="聘用形式" prop="formOfEmployment">
+          <template v-slot="{row}">
+            {{ formEmployees(row.formOfEmployment) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="部门" prop="departmentName" />
+        <el-table-column sortable label="入职时间" prop="timeOfEntry" />
+        <el-table-column label="操作">
+          <el-button type="text">查看</el-button>
+          <el-button type="text">分配角色</el-button>
+        </el-table-column>
+
+      </el-table>
+    </el-card>
   </div>
+
 </template>
 
 <script>
+import { Getemploy } from '@/api/employees'
+// 枚举数据
+import employees from '@/constant/employees'
+
 export default {
+  data() {
+    return {
+      Employees: [],
+      total: 1,
+      q: {
+        page: 1,
+        size: 10
+      }
+    }
+  },
+  created() {
+    this.employ()
+  },
+  methods: { // 打开页面就获取数据
+    async employ() {
+      // 调用接口
+      const res = await Getemploy(this.q)
+
+      // 返回数据等于自定义的空壳
+      this.Employees = res.data.rows
+      console.log(this.Employees)
+
+      // 总条数
+      this.total = res.data.total
+    },
+    formEmployees(code) { // 调用constant/employees文件里面的hireType方法,同通过传递来的id去里面找对应的中文并渲染
+      const res = employees.hireType.find(item => item.id === code).value
+      console.log(res)
+      if (res) {
+        return res
+      } else {
+        return '未知'
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+.num {
+  width: 50px;
+  height: 50px;
+  border-radius:50% ;
+}
 </style>
